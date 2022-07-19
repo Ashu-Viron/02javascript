53ef7c3238cc7ff1e7807b684be13e2150c54bfa
Ashu <97926707+Ashu-Viron@users.noreply.github.com>
2022-07-19 12:42:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//nn bb ss u -Primite in Js
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("257")+BigInt("3")
+let e="Abhimanyu"
+let f=Symbol("I am good boy")
+let g=undefined
+let h
+console.log(a,b,c,d,e,f,g)
+console.log(typeof b)
+console.log(typeof h)
+//Non-Primitive DataType in Js
+const item={
+  "Abhimanyu": true,
+  "Shubh": false,
+  "Ashu": 23,
+  "Rohan": undefined,
+}
+console.log(item["Abhimanyu"])
